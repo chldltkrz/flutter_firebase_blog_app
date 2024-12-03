eb24973ff80d160c1c829a9b1ca10412b17b639c
Issac Choi <chldltkrz@gmail.com>
2024-12-03 16:02:56 +0900

    initial commit

---
+++ lib/ui/write/write_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WritePage extends StatelessWidget {
+  const WritePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text('Write Page'),
+      ),
+    );
+  }
+}
